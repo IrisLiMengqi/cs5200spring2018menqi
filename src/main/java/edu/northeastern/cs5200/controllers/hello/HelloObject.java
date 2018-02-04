b2fb4c3a176c399f1c2a2ba05e5adb9614c1b926
Iris <Iris@IrisMacBook-Pro.local>
2018-02-03 19:43:46 -0500

    pr2 rest api

--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -1,5 +1,25 @@
 package edu.northeastern.cs5200.controllers.hello;
-public class HelloObject {
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+
+@Entity(name="hello")
+public class HelloObject { 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	public int getId() {
+	  return id;
+	}
+	public void setId(int id) {
+	  this.id = id;
+	}
+	
 	private String message;
 	public String getMessage() {
 		return message;
@@ -13,3 +33,9 @@ public class HelloObject {
 	public HelloObject() {
 	}
 }
+//
+//
+//public class HelloObject {
+
+//}
+
